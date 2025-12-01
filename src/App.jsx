72acc47355f33e8510a8cef77e5c21c69be9a0db
Ashrith Varghese <asrithalappat@gmail.com>
2025-12-01 20:27:13 +0530

    Added vercel.json

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
-import { useState } from 'react'
+import { Routes, Route } from 'react-router-dom'
+
+import Login from './pages/Login.jsx'
 
 function App() {
-
   return (
     <>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+      </Routes>
     </>
   )
 }
